325ebf4edc4270cddd1291e5727bb2db1fb5f205
Wesley Moy <wesmoy@uw.edu>
2018-12-02 22:44:33 -0600

    Initial Commit

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ if __name__ == "__main__":
     # 1. Retrieve a list of "events" associated with the given user name
     # 2. Print out the time stamp associated with the first event in that list.
 
-    print("COMPLETE THE TODOs")
+    response = requests.get("https://api.github.com/users/{}/events".format(username))
+    events = json.loads(response.content)
+
+    print(events[0]['created_at'])
     
 
 
